3b16793a7f86f12b1e092b31df8c8d783c046e82
Qiushi Huang <q23huang@uwaterloo.ca>
2026-07-22 23:00:08 -0400

    Add HTTP request paraing

--- server.py
+++ server.py
@@ -7,18 +7,30 @@ print("Server is listening...")
 while True:
     client, address = server.accept()
 
-    print("someone connected!")
+    print("\nsomeone connected!")
     print(address)
 
-    request = client.recv(1024)
+    request = client.recv(1024).decode()
 
-    response = """HTTP/1.1 200 ok
-Content-Type: text/html
+    request_line = request.splitlines()[0]
+    method, path, version = request_line.split()
+    
+    print(method)
+    print(path)
+    print(version)
 
-<h1>Hello World</h1>
-<h2>My First Web Server</h2>
-<p>I build this with python Socket</p>
-"""
+
+
+    response = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "\r\n"
+        "<h1>Hello World</h1>"
+        "<h2>My First Web Server</h2>"
+        "<p>I built this with Python Socket</p>"
+    )
+
+
 
     client.send(response.encode())
     client.close()
